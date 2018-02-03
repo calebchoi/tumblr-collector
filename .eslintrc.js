5cc0f64f34aeff11da52022e9e39b58beb0d7e10
Caleb Choi <calebc1205@gmail.com>
2018-02-02 20:12:31 -0800

    configured eslint to ignore test files

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,10 @@ module.exports = {
     "extends": "airbnb",
     "rules": {
       "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-    }
+    },
+    "overrides": [
+      {
+        "excludedFiles": "*.test.js",
+      }
+    ]
 };
