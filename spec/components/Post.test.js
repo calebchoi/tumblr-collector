487cc3d79e4d4516eb74cb3a084097f0a08582df
Caleb Choi <calebc1205@gmail.com>
2018-02-02 20:11:49 -0800

    implemented sample tests

--- spec/components/Post.test.js
+++ spec/components/Post.test.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import Post from '../../client/src/components/Post';
+import testData from '../testData';
 
-beforeEach(() => {
-  const component = shallow(<Post />);
+Enzyme.configure({ adapter: new Adapter() });
+
+const component = shallow(
+  <Post
+    post={testData.text}
+    id={testData.text.id}
+    addOrRemove={() => {}}
+    favorite={false}
+    type="text"
+  />);
+
+test('Post content should be text for type text', () => {
+  expect(component.find('Text').length).toEqual(1);
 });
 
-test('Post', () => {
-  expect(component).toBe.defined;
+test('Post component should have add button if not favorited', () => {
+  expect(component.find('.add-favorite').length).toEqual(1);
+});
+
+test('Post component should have remove button if not favorited', () => {
+  const favorited = shallow(
+    <Post
+      post={testData.text}
+      id={testData.text.id}
+      addOrRemove={() => {}}
+      favorite={true}
+      type="text"
+    />);
+
+  expect(favorited.find('.remove-favorite').length).toEqual(1);
 });
